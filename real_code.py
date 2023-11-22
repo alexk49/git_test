2c4f8f61042229a9578d6956b3fde6d23f19b802
alexk49 <alexcode9@mailbox.org>
2023-11-22 08:28:04 +0000

    edit file name and add hello world func

---
+++ real_code.py
@@ -0,0 +1,28 @@
+import sys
+
+def hello_world():
+    print("hello world")
+
+def dog():
+    print("woof") * 4
+
+
+def default():
+    print("Hello ")
+
+
+def newFeature():
+    default()
+    print("hello again")
+
+
+def main():
+    if len(sys.argv) == 2:
+        dog()
+    else:
+        default()
+        newFeature()
+
+
+if __name__ == "__main__":
+    main()
